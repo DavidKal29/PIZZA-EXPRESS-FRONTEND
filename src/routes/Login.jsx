cd979c9d4b7ce982fbbf614c8a5066fb4a00cb95
DavidKal29 <davidkalmuk65@gmail.com>
2025-08-22 12:32:46 +0200

    Realizamos lo mismo con el login

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Login() {
 
@@ -6,9 +6,43 @@ export default function Login() {
     document.title = 'Login'
   })
 
+  const [form,setForm] = useState({
+    email:'',
+    username:'',
+    password:''
+  })
+
+  const handleChange = (e)=>{
+    setForm({
+      ...form,
+      [e.target.name]:e.target.value
+    })
+  }
+
+  const loginHandler = async(e)=>{
+    e.preventDefault()
+
+    const res = await fetch('http://localhost:5000/login',{
+      method:'POST',
+      header:{'Content-Type':'application/json'},
+      body:JSON.stringify(form)
+    })
+
+    const data = await res.json()
+
+    const respuesta = JSON.stringify(data)
+
+    console.log('La repsuesta del backend',respuesta);
+    
+
+  }
+
   return (
-    <div className='mt-[80px] mt:my-[100px]'>
-      
-    </div>
+    <form className='mt-[200px]' onSubmit={loginHandler}>
+      <input type="text" name='email' value={form.email} onChange={handleChange} placeholder='Email' />
+      <input type="text" name='username' value={form.username} onChange={handleChange} placeholder='Username' />
+      <input type="text" name='password' value={form.password} onChange={handleChange} placeholder='Password' />
+      <button type='submit'>Enviar</button>
+    </form>
   )
 }
