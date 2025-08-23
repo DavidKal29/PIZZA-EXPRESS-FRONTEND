32a2fc4874ba55a8560cdd23cf253e6da22773b7
DavidKal29 <davidkalmuk65@gmail.com>
2025-08-23 11:58:52 +0200

    Dejamos bien arreglado el banner

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -8,7 +8,10 @@ export default function Home() {
 
   return (
     <div className='mt-[80px] mt:my-[100px]'>
+      <div className='w-full flex justify-center items-center'>
+        <img src="./banner.png" className='lg:w-full lg:h-[45rem]' alt="" />
 
+      </div>
 
     </div>
   )
