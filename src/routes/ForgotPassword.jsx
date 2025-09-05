bd89624f870e4221c9f29cd7622806e879ea5b3f
DavidKal29 <davidkalmuk65@gmail.com>
2025-09-05 11:51:42 +0200

    Creamos los estilos en todos los formularios de la web, con la nueva paleta de colores

--- src/routes/ForgotPassword.jsx
+++ src/routes/ForgotPassword.jsx
@@ -57,9 +57,48 @@ export default function ForgotPassword() {
   }
 
   return (
-    <form className='mt-[200px]' onSubmit={forgotPasswordHandler}>
-      <input type="text" name='email' value={form.email} onChange={handleChange} placeholder='Email' />
-      <button type='submit'>Enviar</button>
-    </form>
+    <div className="flex justify-center items-center pt-[150px] pb-[50px] bg-gradient-to-r from-blue-600 to-blue-800 px-4">
+      <form
+        className="bg-white shadow-xl rounded-2xl p-8 w-full max-w-md flex flex-col gap-6"
+        onSubmit={forgotPasswordHandler}
+      >
+        <h1 className="text-3xl font-extrabold text-center text-blue-800">
+          Recuperar Contraseña
+        </h1>
+
+        {/* Email */}
+        <div className="flex flex-col">
+          <label htmlFor="email" className="text-sm font-semibold text-gray-600">
+            Email
+          </label>
+          <input
+            type="text"
+            name="email"
+            value={form.email}
+            onChange={handleChange}
+            placeholder="Introduce tu email"
+            className="mt-1 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+
+
+        {/* Button */}
+        <button
+          type="submit"
+          className="cursor-pointer bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded-lg shadow-md transition-transform transform hover:scale-105"
+        >
+          Enviar
+        </button>
+
+        {/* Recordaste contraseña */}
+        <div className="text-center">
+          <p
+            className="text-sm text-blue-600"
+          >
+            ¿Recordaste la contraseña? <a href="/login" className='text-blue-800 font-bold'>Inicar Sesión</a>
+          </p>
+        </div>
+      </form>
+    </div>
   )
 }
--- src/routes/cambiarPassword.jsx
+++ src/routes/cambiarPassword.jsx
@@ -60,11 +60,81 @@ export default function CambiarPassword() {
   }
 
   return (
-    <form className='mt-[200px]' onSubmit={changePasswordHandler}>
-    <input type="text" name='new_password' value={form.new_password} onChange={handleChange} placeholder='New Password' />
-    <input type="text" name='confirm_password' value={form.confirm_password} onChange={handleChange} placeholder='Confirm Password' />
-    <button type='submit'>Enviar</button>
-    </form>
+    <div className="flex justify-center items-center pt-[150px] pb-[50px] bg-gradient-to-r from-blue-600 to-blue-800 px-4">
+      <form
+        className="bg-white shadow-xl rounded-2xl p-8 w-full max-w-md flex flex-col gap-6"
+        onSubmit={changePasswordHandler}
+      >
+        <h1 className="text-3xl font-extrabold text-center text-blue-800">
+          Cambiar Contraseña
+        </h1>
+
+        {/* Password */}
+        <div className="flex flex-col">
+          <label
+            htmlFor="new_password"
+            className="text-sm font-semibold text-gray-600"
+          >
+            Contraseña
+          </label>
+          <input
+            type="password"
+            name="new_password"
+            value={form.new_password}
+            autoComplete='off'
+            onChange={handleChange}
+            placeholder="Introduce tu contraseña"
+            className="mt-1 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+
+        {/* Password */}
+        <div className="flex flex-col">
+          <label
+            htmlFor="confirm_password"
+            className="text-sm font-semibold text-gray-600"
+          >
+            Contraseña
+          </label>
+          <input
+            type="password"
+            name="confirm_password"
+            value={form.confirm_password}
+            autoComplete='off'
+            onChange={handleChange}
+            placeholder="Introduce tu contraseña"
+            className="mt-1 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+
+        {/* Forgot password */}
+        <div className="text-right">
+          <a
+            href="/forgotpassword"
+            className="text-sm text-blue-600 hover:underline"
+          >
+            Solicitar nuevo enlace
+          </a>
+        </div>
+
+        {/* Button */}
+        <button
+          type="submit"
+          className="cursor-pointer bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded-lg shadow-md transition-transform transform hover:scale-105"
+        >
+          Enviar
+        </button>
+
+        {/* Cambiaste contraseña */}
+        <div className="text-center">
+          <p
+            className="text-sm text-blue-600"
+          >
+            ¿Ya cambiaste la contraseña? <a href="/login" className='text-blue-800 font-bold'>Inicar Sesión</a>
+          </p>
+        </div>
+      </form>
+    </div>
  )
 
 }
